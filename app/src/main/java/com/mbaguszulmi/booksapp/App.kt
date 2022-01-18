8977cb9381d797fe602a087b00a51447d6973ca8
mbaguszulmi <baguszulmi101198@gmail.com>
2022-01-18 23:35:52 +0700

    feat(app): create App class for HiltAndroidApp

---
+++ app/src/main/java/com/mbaguszulmi/booksapp/App.kt
@@ -0,0 +1,9 @@
+package com.mbaguszulmi.booksapp
+
+import android.support.multidex.MultiDexApplication
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : MultiDexApplication() {
+
+}
