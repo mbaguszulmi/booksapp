5457adf18ce8306c6142d7fce7d7b504e093a0f1
mbaguszulmi <baguszulmi101198@gmail.com>
2022-01-18 23:04:13 +0700

    initial commit

---
+++ app/src/main/java/com/mbaguszulmi/booksapp/view/DetailsActivity.kt
@@ -0,0 +1,4 @@
+package com.mbaguszulmi.booksapp.view
+
+class DetailsActivity {
+}
---
+++ app/src/main/java/com/mbaguszulmi/booksapp/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mbaguszulmi.booksapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mbaguszulmi.booksapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
