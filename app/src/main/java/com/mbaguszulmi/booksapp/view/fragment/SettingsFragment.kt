fa7d2d1fa223e39c15258e01628d7821f2fa857a
mbaguszulmi <baguszulmi101198@gmail.com>
2022-01-20 08:25:20 +0700

    feat(settings): add language settings

--- app/src/main/java/com/mbaguszulmi/booksapp/view/fragment/SettingsFragment.kt
+++ app/src/main/java/com/mbaguszulmi/booksapp/view/fragment/SettingsFragment.kt
@@ -1,26 +1,65 @@
 package com.mbaguszulmi.booksapp.view.fragment
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.preference.PreferenceFragmentCompat
 import com.mbaguszulmi.booksapp.R
+import java.util.*
+import android.content.res.Resources
+
+import java.util.Locale
+
+import android.app.Activity
+import android.content.res.Configuration
+import android.util.Log
+import androidx.preference.ListPreference
+import android.content.Intent
+
+
+
+
 
 /**
  * A simple [Fragment] subclass.
  * Use the [SettingsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        val languagePreference = findPreference<ListPreference>(getString(R.string.pref_key_language))
+        languagePreference?.setOnPreferenceChangeListener { _, newValue ->
+            Log.d("SettingsFragment", "Language changed to $newValue")
+            setLocale(newValue as String)
+            true
+        }
     }
+
+    private fun setLocale(languageCode: String) {
+        val locale = Locale(languageCode)
+        Locale.setDefault(locale)
+        val resources = requireActivity().resources
+        val config: Configuration = resources.configuration
+        config.setLocale(locale)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            requireActivity().createConfigurationContext(config)
+        }
+        resources.updateConfiguration(config, resources.displayMetrics)
+
+        // relaunch app
+        val intent = requireContext().packageManager.getLaunchIntentForPackage(
+            requireContext().packageName
+        )
+        intent?.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        intent?.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
+    }
+
 
     companion object {
         /**
